90cf96aae26e0be520e14c35e38d25144f79c3ed
Johnathan Brooks <jbrooks93@gmail.com>
2020-03-23 02:22:08 -0700

    Added backoff and MySQL connectivity logic

--- src/component.py
+++ src/component.py
@@ -4,9 +4,10 @@ Executes component endpoint executions based on dependent api_mappings.json file
 a set structure, see example below:
 
 TODO: Response for table mappings
+TODO: Integrate SSL, if all else works and there is a need
 """
-
 import logging
+import os
 import sys
 
 # from datetime import datetime
@@ -18,36 +19,56 @@ from kbc.env_handler import KBCEnvHandler
 # from mysql.client import HubspotClient
 # from mysql.result import DealsWriter
 
-KEY_ENDPOINTS = ''
-KEY_API_TOKEN = ''
+logging.basicConfig(level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
+
+current_path = os.path.dirname(__file__)
+module_path = os.path.dirname(current_path)
+
+# Define mandatory parameter constants, matching Config Schema.
+KEY_OBJECTS_ONLY = 'fetchObjectsOnly'
+KEY_MYSQL_HOST = 'host'
+KEY_MYSQL_PORT = 'port'
+KEY_MYSQL_USER = 'username',
+KEY_MYSQL_PWD = '#password',
+KEY_USE_SSH_TUNNEL = 'sshTunnel'
+
+# Define optional parameters as constants for later use.
+KEY_SSH_HOST = 'sshHost'
+KEY_SSH_PORT = 'sshPort'
+KEY_SSH_PUBLIC_KEY = 'sshPublicKey'
 
 # Keep for debugging
 KEY_STDLOG = 'stdlogging'
 KEY_DEBUG = 'debug'
-MANDATORY_PARS = [KEY_ENDPOINTS, KEY_API_TOKEN]
-MANDATORY_IMAGE_PARS = []
+MANDATORY_PARS = (KEY_OBJECTS_ONLY, KEY_MYSQL_HOST, KEY_MYSQL_PORT, KEY_MYSQL_USER, KEY_MYSQL_PWD, KEY_USE_SSH_TUNNEL)
+MANDATORY_IMAGE_PARS = ()
 
 APP_VERSION = '0.0.1'
 
 
 class Component(KBCEnvHandler):
-
-    def __init__(self, debug=False):
-        KBCEnvHandler.__init__(self, MANDATORY_PARS, log_level=logging.DEBUG if debug else logging.INFO)
+    """Keboola extractor component."""
+    def __init__(self, debug: bool = False, data_path: str = None):
+        KBCEnvHandler.__init__(self, MANDATORY_PARS, data_path=data_path,
+                               log_level=logging.DEBUG if debug else logging.INFO)
         # override debug from config
         if self.cfg_params.get(KEY_DEBUG):
             debug = True
         if debug:
             logging.getLogger().setLevel(logging.DEBUG)
-        logging.info('Running version %s', APP_VERSION)
-        logging.info('Loading configuration...')
+        LOGGER.info('Running version %s', APP_VERSION)
+        LOGGER.info('Loading configuration...')
 
         try:
             self.validate_config()
             self.validate_image_parameters(MANDATORY_IMAGE_PARS)
         except ValueError as err:
-            logging.exception(err)
+            LOGGER.exception(err)
             exit(1)
+
+        # TODO: Update to more clear environment variable; used must set local time to UTC.
+        os.environ['TZ'] = 'UTC'
 
     def run(self):
         """Execute main component extraction process."""
@@ -61,8 +82,14 @@ if __name__ == "__main__":
     else:
         debug_arg = False
     try:
-        comp = Component(debug_arg)
+        # Note: If debugging, run docker-compose instead. Only use below two lines for early testing.
+        debug_data_path = os.path.join(module_path, 'data')
+        comp = Component(debug_arg, data_path=debug_data_path)
         comp.run()
-    except Exception as e:
-        logging.exception(e)
+
+        # TODO: Add standard call back in below once ready to move to Docker.
+        # comp = Component(debug_arg)
+        # comp.run()
+    except Exception as generic_err:
+        LOGGER.exception(generic_err)
         exit(1)
--- src/mysql/client.py
+++ src/mysql/client.py
@@ -1,147 +1,101 @@
-import json
-from collections.abc import Iterable
+"""
+Define MySQL connection, session parameters and backoff configuration.
 
-from kbc.client_base import HttpClientBase
+TODO: Integrate SSL here, if SSL is needed.
+"""
+import backoff
+import logging
+import pymysql
 
-COMPANIES_DEFAULT_COLS = ["additionalDomains", "companyId", "isDeleted", "mergeAudits", "portalId", "stateChanges"]
-COMPANY_DEFAULT_PROPERTIES = ['about_us', 'name', 'phone', 'facebook_company_page', 'city', 'country', 'website',
-                              'industry', 'annualrevenue', 'linkedin_company_page',
-                              'hs_lastmodifieddate', 'hubspot_owner_id', 'notes_last_updated', 'description',
-                              'createdate', 'numberofemployees', 'hs_lead_status', 'founded_year',
-                              'twitterhandle',
-                              'linkedinbio']
+LOGGER = logging.getLogger(__name__)
 
-DEAL_DEFAULT_COLS = ["associations.associatedCompanyIds",
-                     "associations.associatedDealIds",
-                     "associations.associatedVids",
-                     "dealId",
-                     "imports",
-                     "isDeleted",
-                     "portalId",
-                     "properties.dealstage.source",
-                     "properties.dealstage.sourceId",
-                     "properties.dealstage.timestamp",
-                     "properties.dealstage.value",
-                     "properties.dealstage.versions",
-                     "properties.hs_object_id.source",
-                     "properties.hs_object_id.sourceId",
-                     "properties.hs_object_id.timestamp",
-                     "properties.hs_object_id.value",
-                     "properties.hs_object_id.versions",
-                     "stateChanges"]
-DEAL_DEFAULT_PROPERTIES = ['authority', 'budget', 'campaign_source', 'hs_analytics_source', 'hs_campaign',
-                           'hs_lastmodifieddate', 'need', 'timeframe', 'dealname', 'amount', 'closedate', 'pipeline',
-                           'createdate', 'engagements_last_meeting_booked', 'dealtype', 'hs_createdate', 'description',
-                           'start_date', 'closed_lost_reason', 'closed_won_reason', 'end_date', 'lead_owner',
-                           'tech_owner', 'service_amount', 'contract_type',
-                           'hubspot_owner_id',
-                           'partner_name', 'notes_last_updated']
-
-# endpoints
-
-DEALS_ALL = 'deals/v1/deal/paged'
-DEALS_RECENT = 'deals/v1/deal/recent/modified'
-
-COMPANIES_ALL = 'companies/v2/companies/paged'
-COMPANIES_RECENT = 'companies/v2/companies/recent/modified'
-
-MAX_RETRIES = 10
-BASE_URL = 'https://api.hubapi.com/'
-
-COMPANY_PROPERTIES = 'properties/v1/companies/properties/'
+MAX_CONNECT_RETRIES = 7
+BACKOFF_FACTOR = 2
+CONNECTION_TIMEOUT_SECONDS = 30
+READ_TIMEOUT_SECONDS = 3000
 
 
-class HubspotClient(HttpClientBase):
+@backoff.on_exception(backoff.expo, pymysql.err.OperationalError, max_tries=MAX_CONNECT_RETRIES, factor=BACKOFF_FACTOR)
+def connect_with_backoff(connection):
+    connection.connect()
+    with connection.cursor() as cursor:
+        set_session_parameters(cursor, net_read_timeout=READ_TIMEOUT_SECONDS)
+
+    return connection
+
+
+def set_session_parameters(cursor: pymysql.connections.Connection.cursor, wait_timeout: int = 300,
+                           net_read_timeout: int = 60, innodb_lock_wait_timeout: int = 300, time_zone: str = '+0:00'):
+    """Set MySQL session parameters to handle for data extraction appropriately.
+
+    Args:
+        cursor: MySQLDB connection cursor object for the active connection.
+        wait_timeout: (Optional) Seconds server waits for activity on inactive session before closing, default 300.
+        net_read_timeout: (Optional) Seconds to wait for more data from connection before aborting the read, default 60.
+        innodb_lock_wait_timeout: (Optional) Seconds a transaction waits for a row lock before giving up, default 300.
+        time_zone: (Optional) String representing the session time zone, default is UTC: '+0:00').
+    Returns:
+        None.
     """
-    Basic HTTP client taking care of core HTTP communication with the API service.
+    logged_warnings = []
+    try:
+        cursor.execute('SET @@session.wait_timeout={}'.format(wait_timeout))
+    except pymysql.err.InternalError as internal_err:
+        logged_warnings.append('Could not set session.wait_timeout. Error: ({}) {}'.format(*internal_err.args))
+    try:
+        cursor.execute("SET @@session.net_read_timeout={}".format(net_read_timeout))
+    except pymysql.err.InternalError as internal_err:
+        logged_warnings.append('Could not set session.net_read_timeout. Error: ({}) {}'.format(*internal_err.args))
+    try:
+        cursor.execute('SET @@session.time_zone="{}"'.format(time_zone))
+    except pymysql.err.InternalError as internal_err:
+        logged_warnings.append('Could not set session.time_zone. Error: ({}) {}'.format(*internal_err.args))
 
-    It exttends the kbc.client_base.HttpClientBase class, setting up the specifics for Hubspot service and adding
-    methods for handling pagination.
+    try:
+        cursor.execute('SET @@session.innodb_lock_wait_timeout={}'.format(innodb_lock_wait_timeout))
+    except pymysql.err.InternalError as e:
+        logged_warnings.append(
+            'Could not set session.innodb_lock_wait_timeout. Error: ({}) {}'.format(*e.args)
+        )
 
-    """
+    if logged_warnings:
+        LOGGER.info('Setting session parameters failed for at least one process, which may impact execution speed.')
+        for warn_message in logged_warnings:
+            LOGGER.warning(warn_message)
 
-    def __init__(self, token):
-        HttpClientBase.__init__(self, base_url=BASE_URL, max_retries=MAX_RETRIES, backoff_factor=0.3,
-                                status_forcelist=(429, 500, 502, 504), default_params={"hapikey": token})
 
-    def _get_paged_result_pages(self, endpoint, parameters, res_obj_name, limit_attr, offset_req_attr, offset_resp_attr,
-                                has_more_attr, offset, limit):
-        """
-        Generic pagination getter method returning Iterable instance that can be used in for loops.
+class MySQLConnection(pymysql.connections.Connection):
+    def __init__(self, config):
+        args = {
+            "user": config["user"],
+            "password": config["password"],
+            "host": config["host"],
+            "port": int(config["port"]),
+            "cursorclass": config.get("cursorclass") or pymysql.cursors.SSCursor,
+            "connect_timeout": CONNECTION_TIMEOUT_SECONDS,
+            "read_timeout": READ_TIMEOUT_SECONDS,
+            "charset": "utf8",
+        }
 
-        :param endpoint:
-        :param parameters:
-        :param res_obj_name:
-        :param limit_attr:
-        :param offset_req_attr:
-        :param offset_resp_attr:
-        :param has_more_attr:
-        :param offset:
-        :param limit:
-        :return:
-        """
-        has_more = True
-        while has_more:
+        if config.get("database"):
+            args["database"] = config["database"]
 
-            parameters[offset_req_attr] = offset
-            parameters[limit_attr] = limit
+        super().__init__(defer_connect=True, **args)
 
-            req = self.get_raw(self.base_url + endpoint, params=parameters)
-            resp_text = str.encode(req.text, 'utf-8')
-            req_response = json.loads(resp_text)
+    def __enter__(self):
+        return self
 
-            if req_response[has_more_attr]:
-                has_more = True
-            else:
-                has_more = False
-            offset = req_response[offset_resp_attr]
+    def __exit__(self, *exc_info):
+        del exc_info
+        self.close()
 
-            yield req_response[res_obj_name]
 
-    def get_companies(self, recent=False, fields=None):
+def make_connection_wrapper(config):
+    class ConnectionWrapper(MySQLConnection):
+        def __init__(self, *args, **kwargs):
+            config["cursorclass"] = kwargs.get('cursorclass')
+            super().__init__(config)
 
-        offset = 0
-        if not fields:
-            company_properties = COMPANY_DEFAULT_PROPERTIES
-        else:
-            company_properties = fields
+            connect_with_backoff(self)
 
-        parameters = {'properties': company_properties}
-
-        if recent:
-            return self._get_paged_result_pages(COMPANIES_RECENT, parameters, 'results', 'count', 'offset', 'offset',
-                                                'hasMore', offset, 200)
-        else:
-            return self._get_paged_result_pages(COMPANIES_ALL, parameters, 'companies', 'limit', 'offset', 'offset',
-                                                'has-more', offset, 250)
-
-    def get_company_properties(self):
-        req = self.get_raw(self.base_url + COMPANY_PROPERTIES)
-        req_response = req.json()
-        return req_response
-
-    def get_deals(self, start_time=None, fields=None) -> Iterable:
-        """
-        Get either all available deals or recent ones specified by start_time.
-
-        API supports more options, possible to extend in the future
-        :type fields: list list of deal properties to get
-        :param start_time: datetime
-        :return: generator object with all available pages
-        """
-        offset = 0
-        if not fields:
-            deal_properties = DEAL_DEFAULT_PROPERTIES
-        else:
-            deal_properties = fields
-
-        parameters = {'properties': deal_properties, 'propertiesWithHistory': 'dealstage',
-                      'includeAssociations': 'true'}
-        if start_time:
-            parameters['since'] = int(start_time.timestamp() * 1000)
-            return self._get_paged_result_pages(DEALS_RECENT, parameters, 'results', 'count', 'offset', 'offset',
-                                                'hasMore',
-                                                offset, 100)
-        else:
-            return self._get_paged_result_pages(DEALS_ALL, parameters, 'deals', 'limit', 'offset', 'offset', 'hasMore',
-                                                offset, 250)
+    return ConnectionWrapper
