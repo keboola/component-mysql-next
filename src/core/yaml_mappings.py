5f1a9eafd58a002c67523b069170f5b124fdff2a
Johnathan Brooks <jbrooks93@gmail.com>
2020-08-10 22:28:27 -0700

    Cleanup YAML Mappings file for flake8

--- src/core/yaml_mappings.py
+++ src/core/yaml_mappings.py
@@ -1,18 +1,15 @@
 import json
 import logging
-import yaml
+# import yaml
 
 
 def make_yaml_mapping_file(json_mappings: dict):
     """Creates the input YAML mapping file for choosing desired tables and columns based on mappings JSON."""
     yaml_data = []
     stream_data = json_mappings['streams']
-    full_json_map = {}
     databases_and_tables = {}
 
-    # print(stream_data)
     for stream in stream_data:
-        # print(stream)
         # Iterate through mappings per database
         table_name = stream['stream']
         table_metadata = stream['metadata'][0]['metadata']
@@ -59,24 +56,11 @@ def convert_yaml_to_json_mapping(yaml_mappings, raw_json_mapping):
         yaml_mappings: The YAML mappings to be converted
         raw_json_mapping: The Raw JSON table mappings from the database
     """
-    # with open(yaml_file, 'r') as yaml_input_mapping:
-    #     yaml_mappings = yaml.safe_load(yaml_input_mapping)
-
     stream_data = raw_json_mapping['streams']
-    # print(stream_data)
 
-    # table_metadata = stream['metadata'][0]['metadata']
-    # columns_metadata = stream['metadata'][1:]
-
-    json_data = dict()
     for schema_sets in yaml_mappings:
-        # print('schema sets:')
-        # print(schema_sets)
         for schema_name, table_mappings in schema_sets.items():
-            # print(schema_name)
-            # print(table_mappings)
             for table_sets in table_mappings['tables']:
-                # print(table_sets)
                 for table_name, mappings in table_sets.items():
                     table_json_index = -1
                     for index, stream in enumerate(stream_data):
@@ -105,58 +89,28 @@ def convert_yaml_to_json_mapping(yaml_mappings, raw_json_mapping):
                                 column_name, table_name
                             ))
 
-                    # print(schema_name)
-                    # print(table_name)
-                    # print(mappings)
-                    # table_excluded_columns = []
-                    # for column_name, is_selected in mappings['columns'].items():
-                    #     if not is_selected:
-                    #         table_excluded_columns.append(column_name)
-                    #     print(column_name)
-                    #     print(is_selected)
-                    # print(json_data)
-                    # if json_data.get(schema_name):
-                    #     print(json_data[schema_name])
-                    #     print('append')
-                    #     json_data[schema_name].append({table_name: mappings})
-                    # else:
-                    #     print('new')
-                    #     json_data[schema_name] = [{table_name: mappings}]
-                    #     print(json_data)
-                    # json_data[schema_name] = table_name
-                    # print(table_name)
-                # print(mappings) !
-
-    # print(yaml_mappings)
-    # json_data['streams'] = yaml_mappings
-    # json_data = yaml_mappings
-    #
-    # output_json = json.dumps(json_data)
-    # print('stream data:')
-
     output_stream_mappings = json.dumps({'streams': stream_data})
-    # print(output_stream_mappings)
     return output_stream_mappings
 
-    # with open(new_mappings_file, 'w') as json_output_mapping:
-    #     json.dump(json_data, json_output_mapping)
 
+# if __name__ == '__main__':
+    # table_mappings_file = '{json_file}'
+    # new_yaml_file = '{yaml_file}'
 
-if __name__ == '__main__':
-    yaml_file = '/Users/johnathanbrooks/PycharmProjects/keboola_ex_mysql_nextv2/data/in/files/mappings.yaml'
-    table_mappings_file = '/Users/johnathanbrooks/PycharmProjects/keboola_ex_mysql_nextv2/data/in/files/table_mappings.json'
-
-    new_mappings_file = '/Users/johnathanbrooks/PycharmProjects/keboola_ex_mysql_nextv2/data/in/files/table_mappings_raw.json'
+    # new_mappings_file = '{new_mappings_file}'
 
     # with open(table_mappings_file, encoding='utf-8') as json_input_mapping:
     #     json_mappings = json.load(json_input_mapping)
+
+    # raw_yaml_mapping = make_yaml_mapping_file(json_mappings)
     #
-    # make_yaml_mapping_file(json_mappings, yaml_file)
+    # with open(new_yaml_file, 'w') as yaml_out:
+    #     yaml_out.write(yaml.dump(raw_yaml_mapping))
 
-    with open(yaml_file, encoding='utf-8') as yaml_input_mapping:
-        yaml_mappings = yaml.safe_load(yaml_input_mapping)
-
-    with open(new_mappings_file, encoding='utf-8') as new_raw_mapping_file:
-        new_raw_mappings = json.load(new_raw_mapping_file)
-        json_mapping = convert_yaml_to_json_mapping(yaml_mappings, new_raw_mappings)
-        # print(json_mapping)
+    # with open(yaml_file, encoding='utf-8') as yaml_input_mapping:
+    #     yaml_mappings = yaml.safe_load(yaml_input_mapping)
+    #
+    # with open(new_mappings_file, encoding='utf-8') as new_raw_mapping_file:
+    #     new_raw_mappings = json.load(new_raw_mapping_file)
+    #     json_mapping = convert_yaml_to_json_mapping(yaml_mappings, new_raw_mappings)
+    #     # print(json_mapping)
