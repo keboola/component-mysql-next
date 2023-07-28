6dcb59acd68446f6dade22aebba2c622022b7ac9
David Esner <david@keboola.com>
2023-07-28 13:49:17 +0200

    TIME as string

--- src/core/datatypes.py
+++ src/core/datatypes.py
@@ -2,7 +2,8 @@
 Keboola base data types for manifest files.
 """
 BASE_STRING = {'string', 'char', 'enum', 'longtext', 'mediumtext', 'text', 'year',
-               'varchar', 'set', 'json', 'binary', 'varbinary', 'blob', "tinyblob", "blob", "mediumblob", "longblob"}
+               'varchar', 'set', 'json', 'binary', 'varbinary', 'blob', "tinyblob", "blob", "mediumblob", "longblob",
+               "time"}
 BASE_INTEGER = {'integer', 'tinyint', 'smallint', 'mediumint', 'int', 'bigint'}
 BASE_NUMERIC = {'numeric', 'decimal'}
 BASE_FLOAT = {'float', 'double'}
