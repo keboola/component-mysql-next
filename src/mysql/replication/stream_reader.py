ec1b4b930e3d9cf1dab74ea623181ae7fb6398ec
SgtMarmite <d.novotny31415@gmail.com>
2024-04-23 14:55:31 +0200

    remove sleep

--- src/mysql/replication/stream_reader.py
+++ src/mysql/replication/stream_reader.py
@@ -29,7 +29,7 @@ except ImportError:
 # 2013 Connection Lost
 # 2006 MySQL server has gone away
 MYSQL_EXPECTED_ERROR_CODES = [2013, 2006]
-MYSQL_SLEEP_CODE = 4052
+MYSQL_SLAVE_ID_ERROR_CODE = 4052
 SLEEP_FOR = 60
 
 
@@ -466,12 +466,8 @@ class BinLogStreamReaderAlterTracking(BinLogStreamReader):
                     self._stream_connection.close()
                     self._BinLogStreamReader__connected_stream = False
                     continue
-                elif code == MYSQL_SLEEP_CODE:
-                    logging.warning(f"Detected error code {code}. Sleeping for {SLEEP_FOR} seconds before retrying.")
-                    self._stream_connection.close()
-                    self._BinLogStreamReader__connected_stream = False
-                    time.sleep(MYSQL_SLEEP_CODE)
-                    continue
+                elif code == MYSQL_SLAVE_ID_ERROR_CODE:
+                    logging.warning(f"Detected Slave ID error code: {code}.")
                 raise
 
             if pkt.is_eof_packet():
