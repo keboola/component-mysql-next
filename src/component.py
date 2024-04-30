cfc7aaf813fddbd1deed44e63dbd6720a7dbf2a0
SgtMarmite <d.novotny31415@gmail.com>
2024-04-30 12:49:43 +0200

    add _build_unique_server_id method

--- src/component.py
+++ src/component.py
@@ -1240,7 +1240,7 @@ class Component(ComponentBase):
         core.write_message(core.StateMessage(value=copy.deepcopy(state)), message_store=message_store)
 
     @staticmethod
-    def sync_binlog_streams(mysql_conn, binlog_catalog, mysql_config, state,
+    def sync_binlog_streams(mysql_conn, binlog_catalog, mysql_config, state, server_id,
                             message_store: core.MessageStore = None, schemas=[], tables=[], columns={},
                             append_mode=False):
         if binlog_catalog.streams:
@@ -1248,7 +1248,7 @@ class Component(ComponentBase):
                 write_schema_message(stream, message_store=message_store)
 
             with metrics.job_timer('sync_binlog'):
-                binlog.sync_binlog_stream(mysql_conn, mysql_config, binlog_catalog.streams, state,
+                binlog.sync_binlog_stream(mysql_conn, mysql_config, binlog_catalog.streams, state, server_id,
                                           message_store=message_store, schemas=schemas, tables=tables, columns=columns,
                                           is_append_mode=append_mode)
 
@@ -1266,9 +1266,27 @@ class Component(ComponentBase):
 
         binlog_catalog = get_binlog_streams(mysql_conn, catalog, config, state, append_mode)
         logging.info('Number of binlog catalog tables to process: {}'.format(len(binlog_catalog)))
-        self.sync_binlog_streams(mysql_conn, binlog_catalog, mysql_config, state,
+
+        server_id = self._build_unique_server_id()
+
+        self.sync_binlog_streams(mysql_conn, binlog_catalog, mysql_config, state, server_id,
                                  message_store=message_store, schemas=schemas, tables=tables, columns=columns,
                                  append_mode=append_mode)
+
+    def _build_unique_server_id(self) -> int:
+        """
+        Returns unique server id based on the configuration and context in the project it runs to ensure uniqueness.
+        Returns:
+
+        """
+        config_id = self.environment_variables.config_id or 1
+        branch_id = self.environment_variables.branch_id or 2
+        project_id = self.environment_variables.project_id or 3
+
+        # make sure it's not exceeding the max Long value
+        server_id = int(f"{project_id}{config_id}{branch_id}") % (2 ** 63 - 1)
+        logging.info(f"Unique server id: {server_id}")
+        return server_id
 
     @staticmethod
     def log_server_params(mysql_conn):
--- src/mysql/replication/binlog.py
+++ src/mysql/replication/binlog.py
@@ -558,8 +558,10 @@ class ShowBinlogMethodFactory:
         return binlogs
 
 
-def sync_binlog_stream(mysql_conn, config, binlog_streams, state, message_store: core.MessageStore = None,
-                       schemas=[], tables=[], columns={}, is_append_mode: bool = False):
+def sync_binlog_stream(mysql_conn, config, binlog_streams, state, server_id: str,
+                       message_store: core.MessageStore = None, schemas=[], tables=[], columns={},
+                       is_append_mode: bool = False):
+
     last_table_schema_cache = state.get(bookmarks.KEY_LAST_TABLE_SCHEMAS, {})
     binlog_streams_map = generate_streams_map(binlog_streams)
 
@@ -574,13 +576,7 @@ def sync_binlog_stream(mysql_conn, config, binlog_streams, state, message_store:
 
     verify_log_file_exists(binary_logs, log_file, log_pos)
 
-    if config.get('server_id'):
-        server_id = int(config.get('server_id'))
-        logging.info("Using provided server_id=%s", server_id)
-    else:
-        unique_id = uuid.uuid4()
-        server_id = unique_id.int % 10000
-        logging.info("No server_id provided, random server_id will be used: %s", server_id)
+    logging.info(f"Using provided server_id: {server_id}")
 
     connection_wrapper = make_connection_wrapper(config)
 
